1f90bd39a391aeb4672b727f1aa7fb906c006fcd
Abel Thomas <90160007+abelthomas1@users.noreply.github.com>
2021-10-03 18:56:48 -0400

    skeleton of exercise 25

---
+++ exercise25/src/main/java/baseline/Solution25.java
@@ -0,0 +1,24 @@
+package baseline;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Solution25 {
+    public static int passwordValidator(String input) {
+        /*
+        using the parameters given, determine the strength of the password
+        */
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        /*
+        prompt user to input the password
+        use a function to determine the strength
+        print the strength of the password
+        */
+    }
+}
+
