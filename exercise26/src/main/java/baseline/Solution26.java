2b3fce99c5d45e061d527902b7f73a9f0bb3a24f
Abel Thomas <90160007+abelthomas1@users.noreply.github.com>
2021-10-03 19:17:55 -0400

    final exercise26

--- exercise26/src/main/java/baseline/Solution26.java
+++ exercise26/src/main/java/baseline/Solution26.java
@@ -15,7 +15,9 @@ public class Solution26 {
             use the formula given to calculate the number of months till the card is paid off
              */
 
-            return 0;
+            apr = apr / 365;
+            return (int) Math.ceil((-1 / 30.0) * Math.log(1 + balance / payment * (1 - Math.pow(1 + apr, 30))) / Math.log(1 + apr));
+
         }
 
         public static void main(String[] args) {
@@ -24,6 +26,23 @@ public class Solution26 {
             determine the number of the months until the card is paid off
             print the result
              */
+
+            Scanner scanner = new Scanner(System.in);
+            double balance, apr, payment;
+
+            System.out.print("What is your balance? ");
+            balance = scanner.nextDouble();
+
+            System.out.print("What is the APR on the card (as a percent)? ");
+            apr = scanner.nextDouble();
+            apr = apr / 100;
+
+            System.out.print("What is the monthly payment you can make? ");
+            payment = scanner.nextDouble();
+
+            int months = calculateMonthsUntilPaidOff(balance, apr, payment);
+            System.out.println("It will take " + months + " months to pay off this card.");
+
         }
     }
 }
