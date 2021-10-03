afa30345132cfa35f939f0e401d859fdaaeec327
Abel Thomas <90160007+abelthomas1@users.noreply.github.com>
2021-10-03 17:53:13 -0400

    skeleton of solution 24

---
+++ exercise24/src/main/java/baseline/Solution24.java
@@ -0,0 +1,28 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution
+ *  Copyright 2021 Abel Thomas
+ */
+
+package baseline;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Solution24 {
+    boolean isAnagram(String string1, String string2) {
+        /*
+        sort the two strings and determine if they are anagrams of each other
+         */
+        return false;
+    }
+
+
+    public static void main(String[] args) {
+        /*
+        prompt user input for two strings
+        determine if the strings are anagrams of each other
+        print the result
+         */
+
+    }
+}
