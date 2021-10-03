5d711d09c916cc8b9ae02e202dc421747949a2f8
Abel Thomas <90160007+abelthomas1@users.noreply.github.com>
2021-10-03 19:15:58 -0400

    skeleton of exercise26

---
+++ exercise26/src/main/java/baseline/Solution26.java
@@ -0,0 +1,29 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution
+ *  Copyright 2021 Abel Thomas
+ */
+
+package baseline;
+
+import java.util.Scanner;
+
+public class Solution26 {
+    public static class PaymentCalculator {
+
+        public static int calculateMonthsUntilPaidOff(double balance, double apr, double payment) {
+            /*
+            use the formula given to calculate the number of months till the card is paid off
+             */
+
+            return 0;
+        }
+
+        public static void main(String[] args) {
+            /*
+            prompt the user to enter the balance, APR, and monthly payment
+            determine the number of the months until the card is paid off
+            print the result
+             */
+        }
+    }
+}
