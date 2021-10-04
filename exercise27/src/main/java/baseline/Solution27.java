b268a20e5b81c53759e6bd65bed1dc797a44681c
Abel Thomas <90160007+abelthomas1@users.noreply.github.com>
2021-10-03 22:21:18 -0400

    skeleton of exercise 27

---
+++ exercise27/src/main/java/baseline/Solution27.java
@@ -0,0 +1,73 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution
+ *  Copyright 2021 Abel Thomas
+ */
+
+package baseline;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class Solution27 {
+
+    public static void main(String[] args) {
+
+    /*
+    prompt the user to enter the first name, last name, employee ID, and ZIP code
+    validate the input according the parameters given
+    print the appropriate error statements
+    */
+
+    }
+
+    // This method validates all fields
+
+    public static void validateInput(String firstName, String lastName, String zip, String employeeID) {
+        /*
+        function used to validate the user's inpur
+         */
+    }
+
+    // This method validates firstName
+
+    public static boolean validateFirstName(String firstName) {
+        /*
+        make sure the first name is at least two characters long
+        make sure it is filled in
+         */
+
+        return false;
+    }
+
+    // This method validates lastName
+
+    public static boolean validateLastName(String lastName) {
+        /*
+        make sure the last name is at least two characters long
+        make sure it is filled in
+         */
+
+        return false;
+    }
+
+    // This method validates zip
+
+    public static boolean validateZipCode(String zip) {
+        /*
+        make sure the zip code is a 5 digit number
+         */
+
+        return false;
+    }
+
+    // This method validates employeeID
+
+    public static boolean validateEmployeeID(String employeeID) {
+        /*
+        make sure the employee id follows the format given
+         */
+
+        return false;
+    }
+
+}
