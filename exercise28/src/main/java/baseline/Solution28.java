3e423e88e4099b67f27457fe6930fb284f1f23d7
Abel Thomas <90160007+abelthomas1@users.noreply.github.com>
2021-10-04 20:08:18 -0400

    skeleton of exercise 28

---
+++ exercise28/src/main/java/baseline/Solution28.java
@@ -0,0 +1,18 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution
+ *  Copyright 2021 Abel Thomas
+ */
+
+package baseline;
+
+import java.util.Scanner;
+
+public class Solution28 {
+    public static void main(String[] args) {
+        /*
+        prompt the user to enter 5 numbers
+        find the sum of the numbers
+        print the total
+         */
+    }
+}
