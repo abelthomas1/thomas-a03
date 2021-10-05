1f90ef936ac0b7147a57ac0acf3343d3b3e3bfd8
Abel Thomas <90160007+abelthomas1@users.noreply.github.com>
2021-10-04 21:30:07 -0400

    skeleton of exercise 34

---
+++ exercise34/src/main/java/baseline/Solution34.java
@@ -0,0 +1,20 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution
+ *  Copyright 2021 Abel Thomas
+ */
+
+package baseline;
+
+import java.util.*;
+
+public class Solution34 {
+    public static void main(String[] args) {
+
+        /*
+        print a list of names
+        prompt the user to enter a name to remove
+        print the list again with the selected name removed
+         */
+
+    }
+}
