f12c32f0c65754e7d05b6eb53f84c40aee3ae7de
Abel Thomas <90160007+abelthomas1@users.noreply.github.com>
2021-10-04 20:33:21 -0400

    skeleton of exercise 30

---
+++ exercise30/src/main/java/baseline/Solution30.java
@@ -0,0 +1,16 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution
+ *  Copyright 2021 Abel Thomas
+ */
+
+package baseline;
+
+public class Solution30 {
+    public static void main(String[] args) {
+
+        /*
+        generate a multiplication table for the numbers 1-12
+         */
+
+    }
+}
