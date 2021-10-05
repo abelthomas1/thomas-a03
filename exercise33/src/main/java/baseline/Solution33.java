1058f34c972af095c268175dfcd930ca6a060af1
Abel Thomas <90160007+abelthomas1@users.noreply.github.com>
2021-10-04 21:19:31 -0400

    skeleton of exercise33

---
+++ exercise33/src/main/java/baseline/Solution33.java
@@ -0,0 +1,19 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution
+ *  Copyright 2021 Abel Thomas
+ */
+
+package baseline;
+
+import java.util.*;
+
+public class Solution33 {
+    public static void main(String[] args) {
+
+        /*
+        prompt the user to enter a question
+        display either "Yes," "No," "Maybe," or "Ask again later"
+         */
+
+    }
+}
