1f5bf5d7be170fa9457f1eb7813a9a7b5904477c
Abel Thomas <90160007+abelthomas1@users.noreply.github.com>
2021-10-04 20:41:00 -0400

    skeleton of exercise 31

---
+++ exercise31/src/main/java/baseline/Solution31.java
@@ -0,0 +1,18 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution
+ *  Copyright 2021 Abel Thomas
+ */
+
+package baseline;
+
+public class Solution31 {
+    public static void main(String[] args) {
+
+        /*
+        prompt the user for age and resting heart rate
+        use the Karvonen formula to determine the target heart rate based on a range of intensities from 55% to 95%
+        generate a table with the results as shown in the example output
+         */
+
+    }
+}
