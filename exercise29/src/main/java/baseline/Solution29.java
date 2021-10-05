cedd220355841eab0f71636b6959a6876626ba32
Abel Thomas <90160007+abelthomas1@users.noreply.github.com>
2021-10-04 20:21:47 -0400

    skeleton of exercise 29

---
+++ exercise29/src/main/java/baseline/Solution29.java
@@ -0,0 +1,22 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution
+ *  Copyright 2021 Abel Thomas
+ */
+
+package baseline;
+
+import java.util.Scanner;
+
+public class Solution29 {
+    public static void main(String[] args) {
+
+        /*
+        prompt the user for the rate of return
+        if the user enters 0, or a non number, print an error message and try again
+        use the formula to find the number of years required to double the investment
+        print the result
+         */
+
+    }
+}
+
