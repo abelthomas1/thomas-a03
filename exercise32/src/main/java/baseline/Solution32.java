cfca33fcb147e62e7dca5563fe7732944254c0f2
Abel Thomas <90160007+abelthomas1@users.noreply.github.com>
2021-10-04 21:02:50 -0400

    skeleton of exercise32

---
+++ exercise32/src/main/java/baseline/Solution32.java
@@ -0,0 +1,23 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution
+ *  Copyright 2021 Abel Thomas
+ */
+
+package baseline;
+
+import java.util.*;
+
+public class Solution32 {
+    public static void main(String[] args) {
+
+        /*
+        prompt the user to choose the difficulty level
+        the first level of difficulty would be a number between 1 and 10,
+        the second difficulty set would be between 1 and 100,
+        and the third difficulty set would be between 1 and 1000
+        each time the player guesses a number give a hint whether the number is higher or lower than the answer
+        once the player guesses the correct number, print the total number of guesses and prompt to play again
+         */
+
+    }
+}
