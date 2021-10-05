228da2a3ffdae7b291c8be0ed31415cd66f3d66b
Abel Thomas <90160007+abelthomas1@users.noreply.github.com>
2021-10-04 20:35:28 -0400

    final solution 30

--- exercise30/src/main/java/baseline/Solution30.java
+++ exercise30/src/main/java/baseline/Solution30.java
@@ -12,5 +12,11 @@ public class Solution30 {
         generate a multiplication table for the numbers 1-12
          */
 
+        for(int i=1;i<=12;i++){
+            for(int j=1;j<=12;j++){
+                System.out.format("%4d",i*j); // aligns each column
+            }
+            System.out.println(); // adds a new line at the end
+        }
     }
 }
